f5d6198c1b10e302d68b12d4ed7194637ed91bb1
tnejman <01187260@pw.edu.pl>
2026-04-10 16:59:12 +0200

    TFV-12 added spring template from initializr; added separate .properties for dev and prod; added dockerfile and docker-compose

---
+++ src/main/java/com/transitflow/visualization_app/VisualizationAppApplication.java
@@ -0,0 +1,13 @@
+package com.transitflow.visualization_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizationAppApplication.class, args);
+	}
+
+}
