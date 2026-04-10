881376eb98977a7264f3b4c83b70755d7c3abd6a
tnejman <01187260@pw.edu.pl>
2026-04-10 16:59:12 +0200

    TFV-12 added spring template from initializr; added separate .properties for dev and prod; added dockerfile and docker-compose

---
+++ src/test/java/com/transitflow/visualization_app/VisualizationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitflow.visualization_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
