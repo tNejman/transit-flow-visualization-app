a5a954032eababdd056bb60ee9ca95b948c06258
tnejman <01187260@pw.edu.pl>
2026-04-16 15:38:08 +0200

    TFV-13 added data model and mapping for db objects

--- src/main/java/com/transitflow/visualization_app/model/RouteSegment.java
+++ src/main/java/com/transitflow/visualization_app/model/RouteSegment.java
@@ -1,5 +1,52 @@
 package com.transitflow.visualization_app.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Entity
+@Table(
+    name = "Route_Segments",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "unique_route_segment_stations",
+            columnNames = {"station_id_from", "station_id_to"}
+        )
+    }
+)
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class RouteSegment {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id_from", nullable = false)
+    private Station stationFrom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id_to", nullable = false)
+    private Station stationTo;
+
+    protected RouteSegment() {
+
+    }
+
+    public RouteSegment(Station stationFrom, Station stationTo) {
+        this.stationFrom = stationFrom;
+        this.stationTo = stationTo;
+    }
 }
